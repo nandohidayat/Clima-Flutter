d77a26b2045714f47b937a6187675947f0b7bcc2
nandohidayat <mnandohidayat@gmail.com>
2019-06-26 00:34:48 +0700

    update screen with the weather

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -12,9 +12,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double latitude;
-  double longitude;
-
   @override
   void initState() {
     super.initState();
@@ -25,17 +22,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     Location location = Location();
     await location.getCurrentLocation();
 
-    latitude = location.latitude;
-    longitude = location.longitude;
-
     NetworkHelper networkHelper = NetworkHelper(
       url:
-          'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey',
+          'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric',
     );
     var weatherData = await networkHelper.getData();
 
     Navigator.push(
-        context, MaterialPageRoute(builder: (context) => LocationScreen()));
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(
+              locationWeather: weatherData,
+            ),
+      ),
+    );
   }
 
   @override
