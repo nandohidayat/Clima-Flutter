7abc3a16803ff7b575e24189b042e9aefc3a1b72
nandohidayat <mnandohidayat@gmail.com>
2019-06-25 23:35:38 +0700

    get temp, cond, city

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 Location location = Location();
 
@@ -13,11 +15,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
     await location.getCurrentLocation();
     print('${location.latitude} ${location.longitude}');
+  }
+
+  void getData() async {
+    Response response = await get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var temp = jsonDecode(data)['main']['temp'];
+      var condition = jsonDecode(data)['weather'][0]['id'];
+      var city = jsonDecode(data)['name'];
+      print('$temp $condition $city');
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
